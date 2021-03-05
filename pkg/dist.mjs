6cb985792331d3f4e6d5da77db19e74fbd524566
Elliana <me@mause.me>
2021-03-05 22:26:20 +1030

    Extend utilities

--- pkg/dist.mjs
+++ pkg/dist.mjs
@@ -1,17 +1,23 @@
 import esbuild from 'esbuild';
+import chalk from 'chalk';
+const { blueBright } = chalk;
 
 esbuild.serve({
     servedir: '.',
-    onRequest: console.log.bind(console),
+    onRequest({ method, path, status }) {
+        console.log((`${blueBright(method)} ${status} ${path}`));
+    }
 }, {
     entryPoints: ['thing.js'],
     bundle: true,
+    color: true,
     define: {
         global: '{}',
         msCrypto: '{}',
         process: '{}',
         Buffer: '{}'
     },
+    logLevel: 'info',
     outfile: 'bundled.js',
     external: ['fs', 'path', 'child_process']
 }).then(console.log.bind(console));
