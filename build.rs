e93a134d41490ae1fe8856c6d8a30c305df9c5c7
Elliana <me@mause.me>
2021-03-20 11:26:23 +0800

    Getting there

--- build.rs
+++ build.rs
@@ -17,46 +17,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let emar_path =
         which("emar").expect("Couldn't find emar, is the emscripten environment activated?");
 
-    eat(cc::Build::new()
-        .get_compiler()
-        .to_command()
-        .arg("-fvisibility=default")
-        .arg("-fPIC")
-        .arg("-DDUCKDB_NO_THREADS=1")
-        .arg("-sWASM=1")
-        .arg("-DDUCKDB_BUILD_LIBRARY=1")
-        .arg("-sWARN_ON_UNDEFINED_SYMBOLS=1")
-        .arg("-sALLOW_MEMORY_GROWTH=1")
-        .arg("-sUSE_PTHREADS=0")
-        .arg("-sDISABLE_EXCEPTION_CATCHING=0")
-        .arg("-fexceptions")
-        .arg("-Wno-unused-parameter")
-        .arg("--no-entry")
-        .arg("-shared")
-        .arg("src/reexporter.cpp")
-        .arg("-Itarget")
-        .arg(
-            std::env::current_dir()?
-                .join("target/duckdb.wasm")
-                .to_str()
-                .expect("oops"),
-        )
-        .arg("-o")
-        .arg("duckdb.o"));
-
-    eat(std::process::Command::new(emar_path)
-        .arg("rcs")
-        .arg("target/libduckdb.a")
-        .arg("duckdb.o"));
-
-    println!("cargo:rustc-link-lib=static-nobundle=duckdb");
-    println!(
-        "cargo:rustc-link-search={}",
-        std::env::current_dir()?
-            .join("target")
-            .to_str()
-            .expect("aaaaa")
-    );
+    cc::Build::new()
+        .flag("-fvisibility=default")
+        .flag("-fPIC")
+        .flag("-DDUCKDB_NO_THREADS=1")
+        .flag("-sWASM=1")
+        .flag("-DDUCKDB_BUILD_LIBRARY=1")
+        .flag("-sWARN_ON_UNDEFINED_SYMBOLS=1")
+        .flag("-sALLOW_MEMORY_GROWTH=1")
+        .flag("-sUSE_PTHREADS=0")
+        .flag("-sDISABLE_EXCEPTION_CATCHING=0")
+        .flag("-fexceptions")
+        .flag("-Wno-unused-parameter")
+        .flag("--no-entry")
+        .flag("-shared")
+        .file("src/reexporter.cpp")
+        .include("target")
+        // .flag("target/duckdb.wasm")
+        .cpp_link_stdlib("stdc++ target/duckdb.wasm")
+        .no_default_flags(true)
+        .compile("libduckdb.a");
 
     Ok(())
 }
