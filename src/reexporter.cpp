f5100134a72b66fa28c477bb42c28a8a33eb1fd3
Elliana May <me@mause.me>
2021-03-23 17:10:19 +0800

    Try different tactic

--- src/reexporter.cpp
+++ src/reexporter.cpp
@@ -2,29 +2,15 @@
 #include "duckdb.hpp"
 #include <iostream>
 
-struct MaterializedQueryResult {};
-duckdb_state duckdb_translate_result(MaterializedQueryResult *result, duckdb_result *out);
-
 extern "C"
 {
     duckdb_result *query(const char *query)
     {
         duckdb::DuckDB db(nullptr);
         duckdb::Connection con(db);
-        auto result = con.Query(query);
 
         duckdb_result *out = (duckdb_result *)malloc(sizeof(duckdb_result *));
 
-        out->error_message = nullptr;
-        if (result->success)
-        {
-            duckdb_translate_result(result.get(), out);
-        }
-        else
-        {
-            out->error_message = strdup(result->error.c_str());
-        }
-
-        return out;
+        return duckdb_query(con, query, out);
     }
 }
