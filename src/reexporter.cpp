0f722f077d9acb670f258087110e564823449de8
Elliana <me@mause.me>
2021-03-28 19:50:33 +0800

    Add integration on c++ side

--- src/reexporter.cpp
+++ src/reexporter.cpp
@@ -347,8 +347,54 @@ idx_t GetCTypeSize(duckdb_type type)
         return sizeof(const char *);
     }
 }
+
+template <class T>
+T UnsafeFetch(duckdb_result *result, idx_t col, idx_t row) {
+	D_ASSERT(row < result->row_count);
+	return ((T *)result->columns[col].data)[row];
+}
+
+static Value GetCValue(duckdb_result *result, idx_t col, idx_t row)
+{
+    if (col >= result->column_count)
+    {
+        return Value();
+    }
+    if (row >= result->row_count)
+    {
+        return Value();
+    }
+    if (result->columns[col].nullmask[row])
+    {
+        return Value();
+    }
+    switch (result->columns[col].type)
+    {
+    case DUCKDB_TYPE_DATE:
+    {
+        auto date = UnsafeFetch<duckdb_date>(result, col, row);
+        return Value::DATE(date.year, date.month, date.day);
+    }
+    default:
+        return Value();
+    }
+}
+
 extern "C"
 {
+    duckdb_date *duckdb_value_date(duckdb_result *result, idx_t col, idx_t row)
+    {
+        Value val = GetCValue(result, col, row);
+        if (val.is_null)
+        {
+            return nullptr;
+        }
+        else
+        {
+            return val.GetValue<duckdb_date *>();
+        }
+    }
+
     duckdb_state query(DatabaseData *db, const char *query, duckdb_result *out)
     {
         duckdb::Connection con(*db->database);
