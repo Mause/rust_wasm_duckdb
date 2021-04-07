70e614aa2e27e149a283a149b53ac44c1693ba93
Elliana May <me@mause.me>
2021-04-07 11:01:21 +0800

    Use constant arrays

--- src/jse.rs
+++ src/jse.rs
@@ -17,13 +17,13 @@ macro_rules! jse {
             let array = &AlignToSixteen([$($i,)*]);
             let sig = CString::new("i".repeat(LEN)).expect("sig");
             #[link_section = ".em_asm"]
-            static SNIPPET: &'static [u8] = *$js_expr;
+            static SNIPPET: [u8; $js_expr.len()] = *$js_expr;
 
             assert_eq!(SNIPPET[..].last().expect("empty snippet?"), &0);
 
             unsafe {
                 emscripten_asm_const_int(
-                    SNIPPET as *const _ as *const u8,
+                    &SNIPPET as *const _ as *const u8,
                     sig.as_ptr() as *const _ as *const u8,
                     array as *const _ as *const u8,
                 ) as i32
@@ -33,11 +33,11 @@ macro_rules! jse {
     ($js_expr:expr) => {
         {
             #[link_section = ".em_asm"]
-            static SNIPPET: &'static [u8] = *$js_expr;
+            static SNIPPET: [u8; $js_expr.len()] = *$js_expr;
 
             unsafe {
                 emscripten_asm_const_int(
-                    SNIPPET as *const _ as *const u8,
+                    &SNIPPET as *const _ as *const u8,
                     crate::jse::empty_sig.as_ptr() as *const _ as *const u8,
                     std::ptr::null() as *const _ as *const u8,
                 ) as i32
