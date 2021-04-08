af0d6ffa0d3f43a50654cd02bb25d141b15cdf09
Elliana May <me@mause.me>
2021-04-08 18:03:34 +0800

    as_str?

--- src/bin.rs
+++ src/bin.rs
@@ -93,9 +93,9 @@ impl ToString for DbType {
             Time(s) => s,
             Timestamp(s) => s,
             Date(s) => s,
-            Blob(s) => format!("blob len: {}", s.size),
-            Hugeint(s) => format!("{:?}", s),
-            Interval(s) => format!("{:?}", s),
+            Blob(s) => format!("blob len: {}", s.size).as_str(),
+            Hugeint(s) => format!("{:?}", s).as_str(),
+            Interval(s) => format!("{:?}", s).as_str(),
             Unknown(_) => &"unknown",
         };
 
