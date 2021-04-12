af106910e103ea464a7825460038315518e18d67
Elliana <me@mause.me>
2021-04-12 10:52:01 +0800

    Restore null checks

--- src/reexporter.cpp
+++ src/reexporter.cpp
@@ -15,35 +15,62 @@ struct DatabaseData {
   duckdb::DuckDB *database;
 };
 
+template <class T>
+T UnsafeFetch(duckdb_result *result, idx_t col, idx_t row)
+{
+    D_ASSERT(row < result->row_count);
+    return ((T *)result->columns[col].data)[row];
+}
+
+template <class T>
+T lookup(duckdb_result *result, idx_t col, idx_t row)
+{
+    if (col >= result->column_count)
+    {
+        return nullptr;
+    }
+    if (row >= result->row_count)
+    {
+        return nullptr;
+    }
+    if (result->columns[col].nullmask[row])
+    {
+        return nullptr;
+    }
+
+    return UnsafeFetch<T>(result, col, row);
+}
+
 extern "C"
 {
-    void* mallocy() {
-        return calloc(1, sizeof(void*));
+    void *mallocy()
+    {
+        return calloc(1, sizeof(void *));
     }
 
     duckdb_date *duckdb_value_date(duckdb_result *result, idx_t col, idx_t row)
     {
-        return &((duckdb_date *)result->columns[col].data)[row];
+        return lookup<duckdb_date *>(result, col, row);
     }
 
     duckdb_time *duckdb_value_time(duckdb_result *result, idx_t col, idx_t row)
     {
-        return &((duckdb_time *)result->columns[col].data)[row];
+        return lookup<duckdb_time *>(result, col, row);
     }
 
     duckdb_interval *duckdb_value_interval(duckdb_result *result, idx_t col, idx_t row)
     {
-        return &((duckdb_interval *)result->columns[col].data)[row];
+        return lookup<duckdb_interval *>(result, col, row);
     }
 
     duckdb_hugeint *duckdb_value_hugeint(duckdb_result *result, idx_t col, idx_t row)
     {
-        return &((duckdb_hugeint *)result->columns[col].data)[row];
+        return lookup<duckdb_hugeint *>(result, col, row);
     }
 
     duckdb_timestamp *duckdb_value_timestamp(duckdb_result *result, idx_t col, idx_t row)
     {
-        return &((duckdb_timestamp *)result->columns[col].data)[row];
+        return lookup<duckdb_timestamp *>(result, col, row);
     }
 
     void ext_duckdb_close(duckdb_database *database) {
