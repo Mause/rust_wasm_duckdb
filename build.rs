32bfbb2fe19f5e88e4eaa002b67d6768dcff6b12
Elliana May <me@mause.me>
2021-04-13 10:44:33 +0800

    Rewrite build

--- build.rs
+++ build.rs
@@ -1,59 +1,18 @@
 #![feature(try_trait)]
 
-use which::which;
-
-fn eat(command: &mut std::process::Command) {
-    let res = command.output().expect("Compile");
-
-    if !res.status.success() {
-        panic!("{}", String::from_utf8(res.stderr).expect("String"));
-    }
-}
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO: reenable
-    // println!("cargo:rustc-link-lib=static-nobundle=stdc++");
-
-    let emar_path =
-        which("emar").expect("Couldn't find emar, is the emscripten environment activated?");
-
-    eat(cc::Build::new()
-        .get_compiler()
-        .to_command()
-        .arg("-fvisibility=default")
-        .arg("-fPIC")
-        .arg("-DDUCKDB_NO_THREADS=1")
-        .arg("-sWASM=1")
-        .arg("-DDUCKDB_BUILD_LIBRARY=1")
-        .arg("-sWARN_ON_UNDEFINED_SYMBOLS=1")
-        .arg("-sALLOW_MEMORY_GROWTH=1")
-        .arg("-sUSE_PTHREADS=0")
-        .arg("-sDISABLE_EXCEPTION_CATCHING=0")
-        .arg("-fexceptions")
-        .arg("-Wno-unused-parameter")
-        .arg("--no-entry")
-        .arg("-shared")
-        .arg("src/reexporter.cpp")
-        .arg("-Itarget")
-        // .arg("target/duckdb.wasm")
-        .arg("target/duckdb.cpp")
-        .arg("-o")
-        .arg("duckdb.o"));
-
-    println!("{:?}", &emar_path);
-    eat(std::process::Command::new(&emar_path)
-        .arg("rcs")
-        .arg("target/libduckdb.a")
-        .arg("duckdb.o"));
-
-    println!("cargo:rustc-link-lib=static-nobundle=duckdb");
-    println!(
-        "cargo:rustc-link-search={}",
-        std::env::current_dir()?
-            .join("target")
-            .to_str()
-            .expect("aaaaa")
-    );
+    cc::Build::new()
+        .flag("-fvisibility=default")
+        .flag("-fPIC")
+        .flag("-DDUCKDB_NO_THREADS=1")
+        .flag("-DDUCKDB_BUILD_LIBRARY=1")
+        .flag("-fexceptions")
+        .flag("-Wno-unused-parameter")
+        .flag("-shared")
+        .file("src/reexporter.cpp")
+        .flag("-Itarget")
+        .file("target/duckdb.cpp")
+        .compile("duckdb")?;
 
     #[cfg(windows)]
     {
